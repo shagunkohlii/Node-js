086db9771e79b3dd04cc7589025f6d48519537e9
Shagun <rishabkohli4@gmail.com>
2024-01-24 20:22:19 +0530

    short-url_stateless-auth

--- url-shortProject/controllers/user.js
+++ url-shortProject/controllers/user.js
@@ -24,9 +24,8 @@ async function handleUserLogin(req, res) {
             error: "invalid Username or Password.",
         })
     }
-    const sessionId = uuidv4();
-    setUser(sessionId, user)
-    res.cookie('uid', sessionId);
+    const token = setUser(user)
+    res.cookie('uid', token);
 
     res.redirect('/')
 }
--- url-shortProject/service/auth.js
+++ url-shortProject/service/auth.js
@@ -1,11 +1,21 @@
-const sessionIdToUserMap = new Map();
+//  stateless Auth
+const jwt = require('jsonwebtoken')
+const secret = 'shagun@126@';
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user)
+function setUser(user) {
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret)
 }
-
-function getUser(id) {
-    return sessionIdToUserMap.get(id)
+ 
+function getUser(token) {
+    if (!token) return null;
+    try {
+        return jwt.verify(token, secret)
+    } catch (error) {
+        return null
+    }
 
 }
 
